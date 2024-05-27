f27e67a9be9164650aee5e5968535457d53ba70b
dh22659 <dh22659@naver.com>
2024-05-27 17:48:45 +0900

    블로그 코드 작성

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,9 +1,7 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
-import { createPost, getPostById, updatePostById } from '../api';
 import { TAG } from '../api/types';
-import Tag from '../components/Tag.tsx';
 import useGetPostById from '../queries/useGetPostById.ts';
 import useCreatePost from '../queries/useCreatePost.ts';
 import useUpdatePostById from '../queries/useUpdatePostById.ts';
